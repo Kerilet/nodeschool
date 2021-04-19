99eb881256aeb478738d83f46c91080603f31054
Kerilet <jivore78@gmail.com>
2021-04-19 15:03:25 -0300

    koa vem de koala?

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+
+'use strict';
+
+const koa = require('koa')
+const app = new koa()
+
+app.use(function *(){
+  this.body = "Hello World !!!";
+});
+
+app.listen(1234)
+
 const lodash = require('lodash');
 const text = lodash.capitalize('kIrA qUeEn');
 console.log(text);
