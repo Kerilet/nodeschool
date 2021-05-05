294a85e77fbfd2b44af13bb0b78c299cb2e21163
Kerilet <jivore78@gmail.com>
2021-05-05 19:56:40 -0300

    pokemon crud added; folders organized; jojos added

--- src/controllers/pokemons.js
+++ src/controllers/pokemons.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-restricted-syntax */
 /* objective: make the big crud */
 
 const util = require('util');
@@ -5,44 +7,65 @@ const path = require('path');
 const fs = require('fs');
 
 const readDir = util.promisify(fs.readdir);
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
 
 module.exports = {
   async readAll(ctx) {
-    const jsonPath = path.join(__dirname, '../jsons');
+    const jsonPath = path.join(__dirname, '../jsons/pokemons');
     const files = await readDir(jsonPath);
     const { limit = 20, offset = 0 } = ctx.request.query;
     const offseted = files
       .filter((item, index) => index >= offset)
       .filter((item, index) => index < limit);
+    const results = [];
+    for (const filename of offseted) {
+      const buffer = await readFile(path.join(__dirname, `../jsons/pokemons/${filename}`));
+      const result = JSON.parse(buffer.toString());
+      results.push(result);
+    }
     ctx.body = {
       count: files.length,
-      results: offseted.map((file) => {
-        const pokemonName = file.slice(0, -5);
-        return {
-          name: pokemonName,
-          url: `/pokemons/${pokemonName}`,
-        };
-      }),
+      results,
     };
   },
   async readSingle(ctx) {
     try {
-      const json = fs.readFileSync(path.resolve(__dirname, `../jsons/${ctx.params.pokemonName}.json`));
-      ctx.body = json.toString();
+      const json = fs.readFileSync(path.resolve(__dirname, `../jsons/pokemons/${ctx.params.pokemonName}.json`));
+      ctx.body = JSON.parse(json);
     } catch (error) {
       ctx.status = 404;
     }
   },
   async create(ctx) {
     const json = ctx.request.body;
-    // eslint-disable-next-line no-console
-    console.log(json.forms[0].name);
-    ctx.body = 'created yay';
+    const { name } = json.forms[0];
+    await writeFile(`src/jsons/pokemons/${name}.json`, JSON.stringify(json, null, 4));
+    ctx.status = 201;
+    ctx.body = json;
   },
   async update(ctx) {
-    ctx.body = 'updated yay';
+    const { pokemonName } = ctx.params;
+    const filePath = `src/jsons/pokemons/${pokemonName}.json`;
+    try {
+      fs.unlinkSync(filePath);
+      const json = ctx.request.body;
+      await writeFile(`src/jsons/pokemons/${pokemonName}.json`, JSON.stringify(json, null, 4));
+      ctx.body = json;
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error(err);
+    }
   },
   async delete(ctx) {
-    ctx.body = 'deleted yay';
+    const filePath = `src/jsons/pokemons/${ctx.params.pokemonName}.json`;
+    try {
+      fs.unlinkSync(filePath);
+      ctx.status = 204;
+      ctx.body = '';
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error(err);
+    }
   },
 };
--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ const routes = require('./routes');
 const app = new Koa();
 
 app
-  .use(koaBody())
+  .use(koaBody(koaBody({
+    multipart: true,
+    formidable: {
+      maxFileSize: 200 * 1024 * 1024, // Set the maximum upload file size limit, default 2M
+    },
+  })))
   .use(routes.routes())
   .use(routes.allowedMethods())
   .listen(3000);
